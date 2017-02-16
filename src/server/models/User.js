2de23641bba1819a04769038fe0b27b21e7b777e
dongwenxiao <cs_victor@126.com>
2017-02-16 14:39:33 +0800

    finish login

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -1,19 +1,12 @@
 import Model from 'sp-model'
 
+const USER_COLLECTION_NAME = 'user'
+
 export default class User extends Model {
 
-    constructor({
-        id,
-        username,
-        password,
-        role,
-        email,
-        phone,
-        wx_openid,
-        wx_info
-    }) {
+    constructor({ id, username, password, role, email, phone, wx_openid, wx_info }) {
 
-        const USER_COLLECTION_NAME = 'user'
+
         super(USER_COLLECTION_NAME)
 
         this.id = id
@@ -25,4 +18,41 @@ export default class User extends Model {
         this.wx_openid = wx_openid
         this.wx_info = wx_info
     }
+
+    static async login(query, password) {
+
+        const db = await this.dao.getDB()
+        const col = await db.collection(USER_COLLECTION_NAME)
+        const cursor = col.find(query)
+        const users = await cursor.toArray()
+        db.close()
+
+        // 登录用户未找到
+        if (users.length === 0) {
+            return null
+        }
+
+        // 登录用户密码错误
+        const user = users[0]
+        if (user.password !== password) {
+            return false
+        }
+
+        // 登录成功
+        return user
+    }
+
+    static async loginByEmail(email, password) {
+        return await this.login({ email }, password)
+    }
+
+    static async loginByUsername(username, password) {
+        return await this.login({ username }, password)
+    }
+
+    static async loginByPhone(phone, password) {
+        return await this.login({ phone }, password)
+    }
+
+
 }
--- src/server/router/index.js
+++ src/server/router/index.js
@@ -1,7 +1,7 @@
 import Router from 'koa-router'
-// import Role from '../models/Role'
+import User from '../models/User'
 // import _ from 'underscore'
-import {createRouter as createAdminRouter} from './admin'
+import { createRouter as createAdminRouter } from './admin'
 
 const createRouter = (rootRouter) => {
 
@@ -12,12 +12,47 @@ const createRouter = (rootRouter) => {
 
     router
         .get('/register', async(ctx) => {
-            // return ctx.render('home', { name: 'victor' })
-        })
-        .post('/register', async(ctx) => {
+
+            var ss = ctx.spResponse(200, { a: 1 }, 'sss')
+
+            console.log(ss)
+            ctx.body = ss
 
         })
+        .post('/register', async(ctx) => {})
         .get('/login', async(ctx) => {
+
+            const username = ctx.query.username
+            const email = ctx.query.email
+            const password = ctx.query.password
+
+            //
+            let user
+            if (username) user = await User.loginByUsername(username, password)
+            if (email) user = await User.loginByEmail(email, password)
+
+            // 无可用登录信息
+            if (user === undefined) {
+                ctx.spResponse(200, {}, '提供登录信息有误。')
+            }
+
+            // 没找到登录用户
+            if (user === null) {
+                ctx.spResponse(200, {}, '没找到登录用户。')
+            }
+
+            // 密码错误
+            if (user === false) {
+                ctx.spResponse(200, {}, '密码错误。')
+            }
+
+            // 登录成功
+            if (user) {
+                ctx.session.user = user
+
+                console.log(ctx.session)
+                ctx.spResponse(200, user, '登录成功。')
+            }
 
         })
         .post('/login', async(ctx) => {
@@ -29,118 +64,6 @@ const createRouter = (rootRouter) => {
         .post('/forgot', async(ctx) => {
 
         })
-        // .get('/admin/role', async(ctx) => {
-
-        //     // 注册的路由
-        //     let registerRoutes = []
-
-        //     // 二次封装
-        //     rootRouter.root.stack
-        //         .filter((r) => r.methods.length > 0)
-        //         .forEach((r) => {
-        //             r.methods.forEach((m) => {
-        //                 registerRoutes.push({
-        //                     method: m,
-        //                     path: r.path
-        //                 })
-        //             })
-        //         })
-
-        //     // 过滤掉其他
-        //     registerRoutes = registerRoutes.filter((r) => {
-        //         let m = r.method.toUpperCase()
-        //         return !(m === 'HEAD' || m === 'OPTIONS')
-        //     })
-
-        //     return ctx.render('admin/role', {
-        //         routes: registerRoutes,
-        //         roles: ['role1', 'role2']
-        //     })
-        // })
-
-
-    // .get('/admin/role_get', async(ctx) => {
-    //     let _id = ctx.query.id
-    //     let roles = []
-    //     if (_id) {
-    //             // 查找某一个角色
-    //         roles = await Role.get({ _query: { _id } })
-    //     } else {
-    //             // 查找全部角色
-    //         roles = await Role.get()
-    //     }
-
-    //     ctx.body = {
-    //         code: 200,
-    //         data: roles
-    //     }
-
-    // })
-    //     .post('/admin/role_add', async(ctx) => {
-
-    //         let name = ctx.request.body.name
-    //         let apis = ctx.request.body.apis
-
-    //         // 不能添加相同的角色名
-
-    //         const roles = await Role.get({ _query: { name } })
-    //         if (roles.length === 0) {
-    //             const role = new Role({ name, apis })
-    //             const result = await Role.add(role)
-
-    //             ctx.body = {
-    //                 code: 200,
-    //                 data: result
-    //             }
-    //         } else {
-    //             ctx.body = {
-    //                 code: 500,
-    //                 data: {
-    //                     msg: 'Fail: Role name existed.'
-    //                 }
-    //             }
-    //         }
-
-    //     })
-    //     .put('/admin/role_update', async(ctx) => {
-    //         let _id = ctx.request.body.id
-    //         let name = ctx.request.body.name
-    //         let apis = ctx.request.body.apis
-
-    //         // obj -> array
-    //         apis = _.values(apis)
-
-    //         const roles = await Role.get({ _query: { name } })
-
-    //         if (roles.length === 0 || roles[0].name === name) {
-    //             const role = new Role({ name, apis })
-    //             const result = await Role.update({ _id }, role)
-
-    //             ctx.body = {
-    //                 code: 200,
-    //                 data: result
-    //             }
-    //         } else {
-    //             ctx.body = {
-    //                 code: 500,
-    //                 data: {
-    //                     msg: 'Fail: Role name existed.'
-    //                 }
-    //             }
-    //         }
-
-    //     })
-    //     .delete('/admin/role_delete', async(ctx) => {
-    //         let _id = ctx.request.body.id
-
-    //         const result = await Role.delete({ _id })
-
-    //         ctx.body = {
-    //             code: 200,
-    //             data: result
-    //         }
-
-    //     })
 
 
     return router
